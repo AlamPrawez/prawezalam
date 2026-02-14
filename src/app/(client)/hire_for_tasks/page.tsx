9695ac9cfd51e722be12216fbd7097cc488b2b2a
prawez alam <prawezalam9@gmail.com>
2026-02-14 14:50:25 +0545

    none

--- src/app/(client)/hire_for_tasks/page.tsx
+++ src/app/(client)/hire_for_tasks/page.tsx
@@ -69,7 +69,7 @@ export default function HireForTasks() {
       {!response && <div className="w-full">
         <div className="flex-row sm:flex sm:px-30 gap-10">
           <div className="w-full sm:w-5xl">
-            <div className="m-4 sm:mx-auto p-6 pe-12 border border-gray-300 sm:border-0 sm:border-e sm:border-gray-300 rounded-xl sm:rounded-none">
+            <div className="m-4 sm:mx-auto p-6 sm:pe-12 border border-gray-300 sm:border-0 sm:border-e sm:border-gray-300 rounded-xl sm:rounded-none">
               <h2 className="mb-6 text-xl font-semibold text-gray-800">
                 Hire for Task
               </h2>
